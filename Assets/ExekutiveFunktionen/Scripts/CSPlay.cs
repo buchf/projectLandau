25352e373aa94e2924da2d63b66382b71f8c84df
buchfelix <buchfelix97@web.de>
2022-06-21 15:52:56 +0200

    Implement all Cards into the script

--- Assets/ExekutiveFunktionen/Scripts/CSPlay.cs
+++ Assets/ExekutiveFunktionen/Scripts/CSPlay.cs
@@ -60,7 +60,7 @@ public class CSPlay : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        currentTrial = 0;
+        currentTrial = 1;
         currentTask(currentTrial);
     }
 
@@ -72,18 +72,163 @@ public class CSPlay : MonoBehaviour
 
     void currentTask(int currentTrial)
     {
-        if (currentTrial == 0)
+        if (currentTrial == 1)
         {
-            left = two_Flower_Red;
-            middle = three_Hat_Red;
-            right = one_Hat_Blue;
-            targetItem = middle;
-            SpawnFunction(left, middle, right);
+            SpawnFunction(two_Flower_Red, three_Hat_Red, one_Hat_Blue);
+            targetItem = three_Hat_Red;
+        }
+        if (currentTrial == 2)
+        {
+            SpawnFunction(two_Fairy_Yellow, two_Hat_Blue, three_Flower_Yellow);
+            targetItem = two_Fairy_Yellow;
+        }
+        if (currentTrial == 3)
+        {
+            SpawnFunction(one_Fairy_Yellow, two_Flower_Yellow, three_Fairy_Blue);
+            targetItem = one_Fairy_Yellow;
+        }
+        if (currentTrial == 4)
+        {
+            SpawnFunction(two_Fairy_Yellow, three_Flower_Yellow, one_Flower_Red);
+            targetItem = three_Flower_Yellow;
+        }
+        if (currentTrial == 5)
+        {
+            SpawnFunction(one_Fairy_Yellow, two_Fairy_Blue, two_Flower_Red);
+            targetItem = two_Fairy_Blue;
+        }
+        if (currentTrial == 6)
+        {
+            SpawnFunction(three_Flower_Blue, one_Flower_Yellow, three_Fairy_Red);
+            targetItem = three_Flower_Blue;
+        }
+        if (currentTrial == 7)
+        {
+            SpawnFunction(two_Fairy_Blue, three_Flower_Blue, one_Flower_Yellow);
+            targetItem = three_Flower_Blue;
+        }
+        if (currentTrial == 8)
+        {
+            SpawnFunction(two_Hat_Red, three_Hat_Yellow, two_Flower_Blue);
+            targetItem = two_Hat_Red;
+        }
+        if (currentTrial == 9)
+        {
+            SpawnFunction(one_Fairy_Blue, two_Flower_Blue, three_Fairy_Red);
+            targetItem = one_Fairy_Blue;
+        }
+        if (currentTrial == 10)
+        {
+            SpawnFunction(one_Fairy_Blue, one_Hat_Red, three_Flower_Red);
+            targetItem = one_Hat_Red;
+        }
+        if (currentTrial == 11)
+        {
+            SpawnFunction(one_Flower_Blue, three_Flower_Yellow, one_Hat_Red);
+            targetItem = one_Flower_Blue;
+        }
+        if (currentTrial == 12)
+        {
+            SpawnFunction(two_Flower_Blue, two_Hat_Red, three_Fairy_Blue);
+            targetItem = two_Flower_Blue;
+        }
+        if (currentTrial == 13)
+        {
+            SpawnFunction(three_Hat_Red, three_Fairy_Yellow, one_Flower_Red);
+            targetItem = three_Hat_Red;
+        }
+        if (currentTrial == 14)
+        {
+            SpawnFunction(two_Fairy_Red, three_Fairy_Blue, two_Hat_Yellow);
+            targetItem = two_Fairy_Red;
+        }
+        if (currentTrial == 15)
+        {
+            SpawnFunction(three_Fairy_Red, three_Hat_Blue, one_Flower_Blue);
+            targetItem = three_Hat_Blue;
+        }
+        if (currentTrial == 16)
+        {
+            SpawnFunction(one_Hat_Red, two_Fairy_Red, three_Hat_Blue);
+            targetItem = one_Hat_Red;
+        }
+        if (currentTrial == 17)
+        {
+            SpawnFunction(three_Hat_Blue, one_Hat_Yellow, three_Flower_Red);
+            targetItem = three_Hat_Blue;
+        }
+        if (currentTrial == 18)
+        {
+            SpawnFunction(three_Flower_Blue, three_Hat_Yellow, one_Fairy_Yellow);
+            targetItem = three_Hat_Yellow;
+        }
+        if (currentTrial == 19)
+        {
+            SpawnFunction(three_Flower_Yellow, one_Flower_Red, two_Fairy_Red);
+            targetItem = one_Flower_Red;
+        }
+        if (currentTrial == 20)
+        {
+            SpawnFunction(two_Hat_Yellow, two_Flower_Blue, one_Hat_Red);
+            targetItem = two_Hat_Yellow;
+        }
+        if (currentTrial == 21)
+        {
+            SpawnFunction(three_Fairy_Blue, two_Flower_Yellow, three_Hat_Yellow);
+            targetItem = three_Fairy_Blue;
+        }
+        if (currentTrial == 22)
+        {
+            SpawnFunction(two_Fairy_Yellow, one_Fairy_Red, two_Hat_Blue);
+            targetItem = two_Fairy_Yellow;
+        }
+        if (currentTrial == 23)
+        {
+            SpawnFunction(two_Hat_Yellow, three_Fairy_Yellow, one_Fairy_Red);
+            targetItem = three_Fairy_Yellow;
+        }
+        if (currentTrial == 24)
+        {
+            SpawnFunction(three_Hat_Red, one_Hat_Red, three_Fairy_Yellow);
+            targetItem = three_Hat_Red;
+        }
+        if (currentTrial == 25)
+        {
+            SpawnFunction(two_Fairy_Blue, two_Flower_Red, one_Hat_Blue);
+            targetItem = two_Fairy_Blue;
+        }
+        if (currentTrial == 26)
+        {
+            SpawnFunction(two_Flower_Yellow, two_Fairy_Red, one_Hat_Yellow);
+            targetItem = two_Flower_Yellow;
+        }
+        if (currentTrial == 27)
+        {
+            SpawnFunction(one_Flower_Red, two_Flower_Yellow, one_Fairy_Blue);
+            targetItem = one_Flower_Red;
+        }
+        if (currentTrial == 28)
+        {
+            SpawnFunction(one_Fairy_Red, one_Flower_Yellow, two_Hat_Yellow);
+            targetItem = one_Flower_Yellow;
+        }
+        if (currentTrial == 29)
+        {
+            SpawnFunction(two_Hat_Blue, one_Flower_Blue, three_Hat_Yellow);
+            targetItem = two_Hat_Blue;
+        }
+        if (currentTrial == 30)
+        {
+            SpawnFunction(three_Flower_Red, two_Hat_Red, one_Hat_Yellow);
+            targetItem = two_Hat_Red;
         }
 
     }
-    void SpawnFunction(GameObject left, GameObject middle, GameObject right)
+    void SpawnFunction(GameObject leftCard, GameObject middleCard, GameObject rightCard)
     {
+        left = leftCard;
+        middle = middleCard;
+        right = rightCard;
         SpawnLeft(left);
         SpawnMiddle(middle);
         StartCoroutine(Wait(right));
@@ -115,6 +260,12 @@ public class CSPlay : MonoBehaviour
         SpawnRight(right);
     }
 
+    void activateObject(GameObject item)
+    {
+        item.GetComponent<Button>().transition = Selectable.Transition.ColorTint;
+        item.GetComponent<Button>().interactable = true;
+
+    }
     public void Compare(GameObject clicked)
     {
         Debug.Log(targetItem.name);
@@ -128,16 +279,19 @@ public class CSPlay : MonoBehaviour
         {
             Debug.Log("false");
         }
+        activateObject(right);
         currentTrial++;
         StartCoroutine(DespawnObject());
     }
 
     IEnumerator DespawnObject()
     {
-        yield return new WaitForSeconds(1f);
+        
         right.SetActive(false);
         middle.SetActive(false);
         left.SetActive(false);
+        yield return new WaitForSeconds(1f);
         currentTask(currentTrial);
     }
+
 }
