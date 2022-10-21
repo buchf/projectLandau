f767844fd3591aca4d80cbce774d6d0a4bb3ce1e
jonato-bit <84855674+jonato-bit@users.noreply.github.com>
2022-10-21 12:15:32 +0200

    Remove 2 toggles

--- Assets/TextFieldHandler.cs
+++ Assets/TextFieldHandler.cs
@@ -11,21 +11,21 @@ public class TextFieldHandler : MonoBehaviour
 {
    // public static string inputVPN;
     public Experiment experiment;
-    public Measurement measurement;
+    public Measurement efmeasurement;
     public InputField vpnField;
 
     // Start is called before the first frame update
     void Start()
     {
         experiment = FindObjectOfType<Experiment>();
-        measurement = FindObjectOfType<Measurement>();
+        efmeasurement = FindObjectOfType<Measurement>();
 
         if (SceneManager.GetActiveScene().buildIndex == 0)
         {
             vpnField.text = SceneSwitch.inputVPN;
             if (!string.IsNullOrEmpty(vpnField.text))
             {
-                measurement.VPN_Num = Int32.Parse(vpnField.text);
+                efmeasurement.VPN_Num = Int32.Parse(vpnField.text);
                 //experiment.OnVPNChanged(inputVPN);
                 EnableMenu();
             }
